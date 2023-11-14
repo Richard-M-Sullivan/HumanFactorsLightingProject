db7d8e4abc92eb6466bed60991ec29159a791326
Richard <richard.m.sullivan2@gmail.com>
2023-11-14 07:24:14 -0600

    asyncio helloworld

--- LightSwitchApplication.py
+++ LightSwitchApplication.py
@@ -1,1 +1,12 @@
-print('Light Switch Application')
+import asyncio
+import random
+
+def hello_world(loop):
+    print('Hello World')
+    loop.stop()
+
+loop = asyncio.get_event_loop()
+loop.call_soon(hello_world, loop)
+
+loop.run_forever()
+loop.close()
